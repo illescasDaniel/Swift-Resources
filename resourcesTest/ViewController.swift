159e57ae88f52a6c1971b1755250b951a6220673
Daniel Illescas Romero <illescas.daniel@protonmail.com>
2021-12-08 11:57:49 +0100

    Initial Commit

---
+++ resourcesTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  resourcesTest
+//
+//  Created by Daniel Illescas Romero on 8/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
